fc056422ce051b02ad536583ac41b1b239107b57
ThatNintendoNerd <samtheman1026@gmail.com>
2024-02-19 12:43:39 -0800

    Move `CollisionFlags` to a new module

--- lvd_lib/src/objects/collision.rs
+++ lvd_lib/src/objects/collision.rs
@@ -1,7 +1,6 @@
 //! The `Collision` object stores data representing a two-dimensional polygonal collision.
 //! Extra data is stored to define properties of each edge in the collision.
 use binrw::binrw;
-use modular_bitfield::prelude::*;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
@@ -13,10 +12,12 @@ use crate::{
 
 pub mod attribute;
 pub mod cliff;
+pub mod flags;
 pub mod spirits_floor;
 
 use attribute::CollisionAttribute;
 use cliff::CollisionCliff;
+use flags::CollisionFlags;
 use spirits_floor::CollisionSpiritsFloor;
 
 /// An LVD object representing a two-dimensional polygonal collision.
@@ -122,57 +123,6 @@ impl Version for Collision {
             Self::V2 { .. } => 2,
             Self::V3 { .. } => 3,
             Self::V4 { .. } => 4,
-        }
-    }
-}
-
-/// Flags for a [`Collision`] representing the global attributes of a collision.
-#[bitfield]
-#[binrw]
-#[br(map = |f: u32| Self::from_bytes(f.to_le_bytes()))]
-#[bw(map = |f: &Self| u32::from_le_bytes(f.into_bytes()))]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(
-    feature = "serde",
-    serde(from = "CollisionDataFlags", into = "CollisionDataFlags")
-)]
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub struct CollisionFlags {
-    /// Boolean flag determining if the collision is dynamic.
-    pub throughable: bool,
-
-    #[skip]
-    __: B15,
-
-    /// Boolean flag determining if the collision can be dropped through.
-    pub dynamic: bool,
-
-    #[skip]
-    __: B15,
-}
-
-#[cfg(feature = "serde")]
-impl From<CollisionDataFlags> for CollisionFlags {
-    fn from(value: CollisionDataFlags) -> Self {
-        Self::new()
-            .with_throughable(value.throughable)
-            .with_dynamic(value.dynamic)
-    }
-}
-
-#[cfg(feature = "serde")]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-struct CollisionDataFlags {
-    throughable: bool,
-    dynamic: bool,
-}
-
-#[cfg(feature = "serde")]
-impl From<CollisionFlags> for CollisionDataFlags {
-    fn from(value: CollisionFlags) -> Self {
-        Self {
-            throughable: value.throughable(),
-            dynamic: value.dynamic(),
         }
     }
 }
---
+++ lvd_lib/src/objects/collision/flags.rs
@@ -0,0 +1,57 @@
+//! The [`CollisionFlags`] type represents the global attributes of a collision.
+use binrw::binrw;
+use modular_bitfield::prelude::*;
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+/// Flags for a [`Collision`](crate::objects::collision::Collision) representing the global attributes of a collision.
+#[bitfield]
+#[binrw]
+#[br(map = |f: u32| Self::from_bytes(f.to_le_bytes()))]
+#[bw(map = |f: &Self| u32::from_le_bytes(f.into_bytes()))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    serde(from = "CollisionDataFlags", into = "CollisionDataFlags")
+)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct CollisionFlags {
+    /// Boolean flag determining if the collision is dynamic.
+    pub throughable: bool,
+
+    #[skip]
+    __: B15,
+
+    /// Boolean flag determining if the collision can be dropped through.
+    pub dynamic: bool,
+
+    #[skip]
+    __: B15,
+}
+
+#[cfg(feature = "serde")]
+impl From<CollisionDataFlags> for CollisionFlags {
+    fn from(value: CollisionDataFlags) -> Self {
+        Self::new()
+            .with_throughable(value.throughable)
+            .with_dynamic(value.dynamic)
+    }
+}
+
+#[cfg(feature = "serde")]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+struct CollisionDataFlags {
+    throughable: bool,
+    dynamic: bool,
+}
+
+#[cfg(feature = "serde")]
+impl From<CollisionFlags> for CollisionDataFlags {
+    fn from(value: CollisionFlags) -> Self {
+        Self {
+            throughable: value.throughable(),
+            dynamic: value.dynamic(),
+        }
+    }
+}
