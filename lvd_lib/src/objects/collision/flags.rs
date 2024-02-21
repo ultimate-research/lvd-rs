bb9d697f5df33b7e164d63c67a1aa1984dc8ed94
ThatNintendoNerd <samtheman1026@gmail.com>
2024-02-21 15:33:21 -0800

    Fix field documentation for `CollisionFlags`

--- lvd_lib/src/objects/collision/flags.rs
+++ lvd_lib/src/objects/collision/flags.rs
@@ -17,13 +17,13 @@ use serde::{Deserialize, Serialize};
 )]
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub struct CollisionFlags {
-    /// Boolean flag determining if the collision is dynamic.
+    /// Boolean flag determining if the collision can be dropped through.
     pub throughable: bool,
 
     #[skip]
     __: B15,
 
-    /// Boolean flag determining if the collision can be dropped through.
+    /// Boolean flag determining if the collision is dynamic.
     pub dynamic: bool,
 
     #[skip]
