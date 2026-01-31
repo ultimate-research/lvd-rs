b27adf8b2734ded059b42b72a35e2a13facd85af
ThatNintendoNerd <samtheman1026@gmail.com>
2026-01-31 00:38:51 -0800

    Add methods to `Array`

--- lvd_lib/src/array.rs
+++ lvd_lib/src/array.rs
@@ -16,6 +16,7 @@ pub enum Array<T: Version + 'static> {
     /// The first version of the `Array` type.
     #[br(pre_assert(version == 1))]
     V1 {
+        /// The number of elements.
         #[br(temp)]
         #[bw(calc = elements.len() as u32)]
         count: u32,
@@ -26,10 +27,61 @@ pub enum Array<T: Version + 'static> {
     },
 }
 
-impl<T> Version for Array<T>
-where
-    T: Version,
-{
+impl<T: Version> Array<T> {
+    /// Creates a new empty `Array`.
+    pub const fn new() -> Self {
+        Self::V1 {
+            elements: Vec::new(),
+        }
+    }
+
+    /// Returns the number of elements in the array.
+    pub const fn len(&self) -> usize {
+        self.as_slice().len()
+    }
+
+    /// Returns `true` if the array contains no elements.
+    pub const fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Extracts a slice containing the entire array.
+    pub const fn as_slice(&self) -> &[Versioned<T>] {
+        match self {
+            Self::V1 { elements } => elements.as_slice(),
+        }
+    }
+
+    /// Extracts a mutable slice of the entire array.
+    pub const fn as_mut_slice(&mut self) -> &mut [Versioned<T>] {
+        match self {
+            Self::V1 { elements } => elements.as_mut_slice(),
+        }
+    }
+
+    /// Returns a mutable reference to the contents of the array.
+    pub const fn as_mut_vec(&mut self) -> &mut Vec<Versioned<T>> {
+        match self {
+            Self::V1 { elements } => elements,
+        }
+    }
+}
+
+impl<T: Version> Default for Array<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Version> From<Vec<Versioned<T>>> for Array<T> {
+    fn from(vec: Vec<Versioned<T>>) -> Self {
+        Self::V1 {
+            elements: vec,
+        }
+    }
+}
+
+impl<T: Version> Version for Array<T> {
     fn version(&self) -> u8 {
         match self {
             Self::V1 { .. } => 1,
