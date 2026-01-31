d1e5af1422d694f712ab392460440fb28e1b4234
ThatNintendoNerd <samtheman1026@gmail.com>
2026-01-31 00:49:18 -0800

    Implement various traits for `Versioned`

--- lvd_lib/src/version.rs
+++ lvd_lib/src/version.rs
@@ -1,5 +1,7 @@
 //! Basic type-versioning utilities.
 
+use std::ops::{Deref, DerefMut};
+
 use binrw::{BinRead, BinWrite, binrw};
 
 #[cfg(feature = "serde")]
@@ -9,7 +11,8 @@ use serde::{Deserialize, Serialize};
 #[binrw]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(transparent))]
-#[derive(Debug)]
+#[derive(Debug, Default)]
+#[repr(transparent)]
 pub struct Versioned<T: Version> {
     /// The version number of the wrapped value.
     #[br(temp)]
@@ -19,6 +22,27 @@ pub struct Versioned<T: Version> {
     /// The wrapped value.
     #[br(args(version))]
     pub inner: T,
+}
+
+impl<T: Version> Versioned<T> {
+    /// Creates a new `Versioned<T>`.
+    pub const fn new(value: T) -> Self {
+        Self { inner: value }
+    }
+}
+
+impl<T: Version> Deref for Versioned<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T: Version> DerefMut for Versioned<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
 }
 
 /// A trait for determining a type's version.
